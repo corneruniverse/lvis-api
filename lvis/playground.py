1026a01cd27425bcff68663bdbbd2dffa7975a18
Diego V <19864849+mambov@users.noreply.github.com>
2025-01-20 13:15:58 -0600

    feat: update playground as an example

--- lvis/playground.py
+++ lvis/playground.py
@@ -1,14 +1,34 @@
 from lvis import LVIS
 from collections import defaultdict
 
-category_id = 45 # 45 = banana
+
 # category_id = 301 # 301 = convertible
 # category_id = 21 # 21 = armor
-obj = LVIS("../dataset/lvis_v1_train.json")
-images_folder_path = "test/lvis_top1/images/train"
-labels_folder_path = "test/lvis_top1/labels/train"
+train_obj = LVIS("../dataset/lvis_v1_train.json")
+val_obj = LVIS("../dataset/lvis_v1_val.json")
 
+category_id = 45 # 45 = banana
 category_index = 0
 
-# obj.download_images_for_category(images_folder_path, category_id)
-obj.export_labels(labels_folder_path, category_id, category_index)
+images_folder_path = "test/banana_armor/images/train"
+labels_folder_path = "test/banana_armor/labels/train"
+train_obj.download_images_for_category(images_folder_path, category_id)
+train_obj.export_labels(labels_folder_path, category_id, category_index)
+
+images_folder_path = "test/banana_armor/images/val"
+labels_folder_path = "test/banana_armor/labels/val"
+val_obj.download_images_for_category(images_folder_path, category_id)
+val_obj.export_labels(labels_folder_path, category_id, category_index)
+
+category_id = 21 # 21 = armor
+category_index = 1
+
+images_folder_path = "test/banana_armor/images/train"
+labels_folder_path = "test/banana_armor/labels/train"
+train_obj.download_images_for_category(images_folder_path, category_id)
+train_obj.export_labels(labels_folder_path, category_id, category_index)
+
+images_folder_path = "test/banana_armor/images/val"
+labels_folder_path = "test/banana_armor/labels/val"
+val_obj.download_images_for_category(images_folder_path, category_id)
+val_obj.export_labels(labels_folder_path, category_id, category_index)
